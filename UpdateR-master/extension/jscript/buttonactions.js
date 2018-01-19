182475730bec72a828b3713fae3ae50ed698ce8c
Alexandra <boca_alexandra96@yahoo.com>
2018-01-19 14:20:41 +0200

    Add files via upload

---
+++ UpdateR-master/extension/jscript/buttonactions.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded',function()
+{
+	var link = document.getElementById('manage');
+	link.addEventListener('click',function()
+	{
+		var openUrl = "./pages/manage.html";
+		chrome.tabs.create({url: openUrl});
+	});
+
+	var select = document.getElementById('select');
+	select.addEventListener('click',function()
+	{
+		 alert('Now you can select an element !');
+         chrome.tabs.getSelected(null, function(tab){
+    chrome.tabs.executeScript(tab.id, {code: "function clickHandler(e){var elem, evt = e ? e:event;if (evt.srcElement)  elem = evt.srcElement;else if (evt.target) elem = evt.target;alert ('' + 'You clicked the following HTML element:  <'+elem.tagName.toUpperCase() + ' ' + elem.innerHTML +'>')} document.onclick = clickHandler;"}, function(response) {
+        
+            });
+         });
+    });
+       
+});
